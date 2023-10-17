5258becde6843b68ac9b092c9e25e073416e24f6
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-10-17 11:44:02 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,8 +4,8 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 nltk.download('vader_lexicon')
 s = SentimentIntensityAnalyzer()
-st.set_page_config(page_title="Sentiment Analyser (Abhay Nath)")
-st.title("Sentiment Analyser by Abhay Nath")
+st.set_page_config(page_title="Sentiment Analyzer (Abhay Nath)")
+st.title("Sentiment Analyzer by Abhay Nath")
 text = st.text_area("Enter your text:")
 if st.button("Analyze"):
     if text:
