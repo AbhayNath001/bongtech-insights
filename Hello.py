4d1104355a057c3f6a764f2a4a659b5abd50390f
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-10-17 10:57:58 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,25 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import streamlit as st          #pip install streamlit
+import nltk                     #pip install nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-import streamlit as st
-from streamlit.logger import get_logger
+nltk.download('vader_lexicon')
+s = SentimentIntensityAnalyzer()
+st.set_page_config(page_title="Sentiment Analyser (BongTech Insights)")
+st.title("Sentiment Analyser (BongTech Insights)")
+text = st.text_area("Enter your text:")
+if st.button("Analyze"):
+    if text:
+        sentiment_scores = s.polarity_scores(text)
 
-LOGGER = get_logger(__name__)
+        if sentiment_scores['compound'] >= 0.05:
+            sentiment = 'Positive'
+        elif sentiment_scores['compound'] <= -0.05:
+            sentiment = 'Negative'
+        else:
+            sentiment = 'Neutral'
 
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+        st.write("Text: ", text)
+        st.write("Sentiment Scores: ", sentiment_scores)
+        st.write("Sentiment: ", sentiment)
+    else:
+        st.warning("Please enter some text for analysis.")
